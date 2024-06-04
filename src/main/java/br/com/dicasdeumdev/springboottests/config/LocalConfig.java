8fba5feefa6d0cbc30b677c56706b633ef86b074
Rafael Campos <rscampos3@gmail.com>
2024-06-04 18:46:40 -0300

    Perfil de testes

---
+++ src/main/java/br/com/dicasdeumdev/springboottests/config/LocalConfig.java
@@ -0,0 +1,26 @@
+package br.com.dicasdeumdev.springboottests.config;
+
+import br.com.dicasdeumdev.springboottests.domain.User;
+import br.com.dicasdeumdev.springboottests.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Bean
+    public void startDB(){
+        User u1 = new User(null, "Valdir", "valdir@email.com", "34jif");
+        User u2 = new User(null, "Rodrigo", "rod@email.com", "34jif");
+        userRepository.saveAll(List.of(u1, u2));
+    }
+}
