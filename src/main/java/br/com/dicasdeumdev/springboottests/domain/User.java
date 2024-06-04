da54723a935ebd0968f20b15cd628799e526bdbc
Rafael Campos <rscampos3@gmail.com>
2024-06-04 17:59:34 -0300

    Classe de User

--- src/main/java/br/com/dicasdeumdev/springboottests/domain/User.java
+++ src/main/java/br/com/dicasdeumdev/springboottests/domain/User.java
@@ -8,13 +8,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-
 @Entity
 public class User {
-
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String name;
 
     @Column(unique = true)
